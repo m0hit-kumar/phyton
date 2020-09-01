8b8b783f2d29103be364292487cd616d86a46a90
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2020-09-01 21:05:36 +0530

    Create greater_number_from_three.py

---
+++ greater_number_from_three.py
@@ -0,0 +1,13 @@
+a=int(input("enter number one"))
+b=int(input("enter number two"))
+c=int(input("enter nuber three"))
+if a>b :
+    if a>c :
+        print("a is greater")
+    else :
+        print("c is greater")
+
+if b>c :
+    print("b is greater")
+else :
+    print("c is greater")

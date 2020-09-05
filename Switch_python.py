a76d76dd72e9965ea690ab10c7aec632296b76b5
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2020-09-05 21:30:19 +0530

    Update and rename Were to Switch_python.py

---
+++ Switch_python.py
@@ -0,0 +1,16 @@
+def switch_demo(argument):
+    switcher = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    }
+    print switcher.get(argument, "Invalid month") 
